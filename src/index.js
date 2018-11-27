ac7d92fa942c50a73cc6c79e23bd8804953ef116
Raman Plaha <ramanplaha1990@gmail.com>
2018-11-27 16:26:50 +0000

    integrating support for redux, server side redux config, displaying api results on page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
+const store = createStore();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
