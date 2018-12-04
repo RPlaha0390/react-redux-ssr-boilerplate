32dbbca046d6e3141498c38cacb52e93f6d9964f
Raman Plaha <rplaha@fadev.local>
2018-12-04 16:29:04 +0000

    add header component to app, create action creater for fetchCurrentUser and create authReducer

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,6 +2,7 @@
  * Declare types 
  */
 export const FETCH_USERS = 'fetch_users';
+export const FETCH_CURRENT_USER = 'fetch_current_user';
 
 /**
  * Action creators
@@ -11,6 +12,16 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
 
   dispatch({
     type: FETCH_USERS,
+    payload: res
+  })
+};
+
+
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get('/current_user')
+
+  dispatch({
+    type: FETCH_CURRENT_USER,
     payload: res
   })
 };
