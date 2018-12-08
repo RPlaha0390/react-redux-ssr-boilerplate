f01b4c05d61ab19736788fb1f94919f95c70f5f8
Raman Plaha <ramanplaha@Ramans-MacBook-Pro-2.local>
2018-12-08 22:10:08 +0000

    install styled components v4, add some page style, update react react-dom 16 to 16.6 for compatability

--- src/client/App.js
+++ src/client/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { renderRoutes } from 'react-router-config';
 import Header from './containers/Header';
 import { fetchCurrentUser } from './actions';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  }
+`;
+
+export const Container = styled.div`
+  width: 960px;
+  margin: 0 auto;
+`;
 
 const App = ({ route }) => {
   return (
     <div>
+      <GlobalStyle />
       <Header />
-      {renderRoutes(route.routes)}
+      <Container>
+        {renderRoutes(route.routes)}
+      </Container>
     </div>
   );
 };
--- src/client/containers/Header.js
+++ src/client/containers/Header.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { Container } from '../App';
 
 const Header = ({ auth }) => {
+
+  const Nav = styled.div`
+    background: #db7093;
+    padding: 15px 0;
+    margin: 0 0 15px 0;
+  `;
+
+  const Title = styled.h1`
+    font-size: 24px;
+    margin-top: 0;
+  `;
+
+  const NavLink = styled(Link)`
+    color: #ffffff;
+    text-decoration: none;
+    display: inline-block;
+    padding-right: 15px;
+  `;
+
+  const AuthLink = styled.a`
+    text-decoration: none;
+    color: #8e3b56;
+    font-weight: bold;
+    float: right;
+  `;
+
   const authButton = auth ? (
-    <a href="/api/logout">Logout</a>
+    <AuthLink href="/api/logout">Logout</AuthLink>
   ) : (
-    <a href="/api/auth/google">Login</a>
+    <AuthLink href="/api/auth/google">Login</AuthLink>
   );
 
   return (
-    <div>
-      <Link to="/">React SSR</Link>
-      <div>
-        <Link to="/users">Users</Link>
-        <Link to="/admins">Admins</Link>
+    <Nav>
+      <Container>
+        <Title>
+          <NavLink to="/">React SSR</NavLink>
+        </Title>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/admins">Admins</NavLink>
         { authButton }
-      </div>
-    </div>
+      </Container>
+    </Nav>
   )
 }
 
